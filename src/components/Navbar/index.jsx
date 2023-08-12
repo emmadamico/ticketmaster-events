9fe74c89c92cc768283f4a1fea79f1762d9ee8f3
Emmanuel Nenadovit <emmanuelnenadovit@gmail.com>
2023-08-12 17:00:20 -0300

    First commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <div>
+            <p>Navbar</p>
+            <input placeholder="Busco tu evento favorito"></input>
+        </div>
+    );
+};
+
+export default Navbar;
