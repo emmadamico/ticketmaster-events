304a3ba06c27c7f86788ee4e312a0e9b1657aabf
Emmanuel Nenadovit <emmanuelnenadovit@gmail.com>
2023-08-13 20:21:24 -0300

    Page is working now. Added like and unlike. Added new hook. Added logic to global storage (using zustang) and local storage

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,18 +1,33 @@
 import styles from './EventItem.module.css';
 
+import useLikeEvents from '../../../../hooks/useLikeEvents';
+import HearthFilled from '../../../../assets/hearth-filled.png';
+import HearthUnfilled from '../../../../assets/hearth-unfilled.png';
+
 const EventItem = ({ info, id, name, image, onEventClick }) => {
-    const handleSeeMoreClick = (event) => {
-        event.stopPropagation();
+    const { isEventLiked, toggleEventLike } = useLikeEvents(id);
+    
+    const handleSeeMoreClick = (evt) => {
+        evt.stopPropagation();
         onEventClick(id);
+    };
+
+    const handleHearthClick = () => {
+        toggleEventLike();
     };
     
     return (
         <div className={`${styles.eventItemContainer} ${styles.anotherContainer}`}>
-            <img src={image} alt={name} width={200} height={200} />
+            <div className={styles.imageContainer}>
+                <img src={isEventLiked ? HearthFilled : HearthUnfilled} alt="Hearth button" className={styles.hearthImage} onClick={handleHearthClick} />
+                <img src={image} alt={name} width={200} height={200} />
+            </div>
             <div className={styles.eventInfoContainer}>
                 <h4 className={styles.eventName}>{name}</h4>
                 <p className={styles.eventInfo}>{info}</p>
-                <button onClick={handleSeeMoreClick} className={styles.seeMoreBtn}>Ver mas</button>
+                <button onClick={handleSeeMoreClick} className={styles.seeMoreBtn}>
+                    Ver mas
+                </button>
             </div>
         </div>
     );
