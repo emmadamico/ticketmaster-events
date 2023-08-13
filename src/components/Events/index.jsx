f6bafe9a872eb79659720c45fb375a57a0f223c3
Emmanuel Nenadovit <emmanuelnenadovit@gmail.com>
2023-08-12 21:24:51 -0300

    First events management

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -7,13 +7,19 @@ const Events = () => {
     const [data] = useState(eventsJSON);
     const events = data._embedded.events;
     //Tmb se puede hacer asi: const { _embedded: { events }} = data;
-    
+
+    const handleEventItemClick = (id) => {
+        console.log('evento clickeado: ', id);
+    };
+
     const eventsComponent = events.map((eventItem) => (
         <EventItem 
             key={`event-item-${eventItem.id}`} 
             name={eventItem.name}
             info={eventItem.info}
             image={eventItem.images[0].url}
+            onEventClick={handleEventItemClick}
+            id={eventItem.id}
         />
     ));
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 const Navbar = () => {
+    const [search, setSearch] = useState(''); 
+
+    const handleInputchange = (event) => {
+        setSearch(event.target.value);
+    };
+    
     return (
         <div>
             <p>Navbar</p>
-            <input placeholder="Busco tu evento favorito"></input>
+            <input 
+                placeholder="Busco tu evento favorito" 
+                onChange={handleInputchange}
+                value={search} 
+            />
         </div>
     );
 };
